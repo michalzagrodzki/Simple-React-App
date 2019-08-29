5b09adec83244b6c1c5c87cd18a4df01710ae5ce
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-08-29 23:54:46 +0200

    Remove unnecessary constructors

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
   componentDidMount() {
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
   componentDidMount() {
 
