48b43a69f8e4255fc4ec738358ea95798576bd9e
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-09-01 16:49:46 +0200

    Create outline for test of Main component

---
+++ src/tests/Main.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Main from './../components/Main';
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders with or without a name", () => {
+  act(() => {
+    render(<Main />, container);
+  });
+  console.log('showing content of Main container');
+	console.log(container);
+  // expect(container.textContent).toBe("Hey, stranger");
+});
+
