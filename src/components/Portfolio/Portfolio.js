5623b28ccc6446aa747d46bd95c6a5a912fcd34d
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-09-03 20:24:21 +0200

    Create outline for Portfolio component. Create props and state and fetch data to component.

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,13 +1,56 @@
 import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom'
 
 class Portfolio extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      subtitle: '',
+      products: [],
+      error: {
+        message: ''
+      }
+    };
+  }
+
+  CancelToken = axios.CancelToken;
+  source = this.CancelToken.source();
+
+  getProducts () {
+    axios.get('/assets/JSON/products.json', 
+      { 
+        cancelToken: this.source.token 
+      })
+      .then(response => {
+        this.setState({
+          products: response.data
+        })
+      },
+      (error) => {
+        if (axios.isCancel(error)) {
+          console.log('Request canceled: ' + error.message);
+        } else {
+          this.setState({
+            error: {
+              message: error 
+            }
+          });
+        }
+      });
+  }
 
   componentDidMount() {
-
+    this.getProducts();
+    this.setState({
+      title: 'Products Portfolio',
+      subtitle: 'Complete list of my products'
+    });
   }
 
   componentWillUnmount() {
-
+    this.source.cancel("All operations cancelled.");
   }
 
   render() {
