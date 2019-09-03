636a571902bbe293ff8cc101e32b42d003df717c
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-09-03 20:33:28 +0200

    Add styles to Portfolio component

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,6 +2,8 @@ import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom'
 
+import "./portfolio.scss";
+
 class Portfolio extends React.Component {
   constructor() {
     super();
