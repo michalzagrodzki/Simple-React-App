88d5289537c8844fcd46697dbb91cbc66ad48a7c
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-09-05 14:58:04 +0200

    Update tests of Main component to include router

--- src/tests/Main.test.js
+++ src/tests/Main.test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { MemoryRouter } from "react-router-dom";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 
@@ -20,7 +21,12 @@ afterEach(() => {
 
 it("renders with title", () => {
   act(() => {
-    render(<Main />, container);
+    render(
+      <MemoryRouter>
+        <Main />
+      </MemoryRouter>, 
+      container
+    );
   });
   const title = container.querySelector('.head-section h1');
   expect(title.textContent).toBe('Simple React App');
@@ -28,7 +34,12 @@ it("renders with title", () => {
 
 it("renders with first section title", () => {
   act(() => {
-    render(<Main />, container);
+    render(
+      <MemoryRouter>
+        <Main />
+      </MemoryRouter>, 
+      container
+    );
   });
   const title = container.querySelector('.subtitle-header h4');
   expect(title.textContent).toBe('REACT APP');
@@ -36,7 +47,12 @@ it("renders with first section title", () => {
 
 it("renders with main message title", () => {
   act(() => {
-    render(<Main />, container);
+    render(
+      <MemoryRouter>
+        <Main />
+      </MemoryRouter>, 
+      container
+    );
   });
   const title = container.querySelector('.subtitle-body h2');
   expect(title.textContent).toBe('This is example app using react');
@@ -44,7 +60,12 @@ it("renders with main message title", () => {
 
 it("renders with main message sub-title", () => {
   act(() => {
-    render(<Main />, container);
+    render(
+      <MemoryRouter>
+        <Main />
+      </MemoryRouter>, 
+      container
+    );
   });
   const title = container.querySelector('.subtitle-body h3');
   expect(title.textContent).toBe('this app shows capabilities of using react in simple cases');
