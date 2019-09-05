c85cbfa254bd112c9ad463cf6f85bbd1050442db
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-09-05 21:31:00 +0200

    Correct tests for contact component

--- src/tests/Contact.test.js
+++ src/tests/Contact.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
+import { MemoryRouter } from "react-router-dom";
 import { act } from "react-dom/test-utils";
 
 import Contact from './../components/Contact/Contact';
@@ -20,7 +21,12 @@ afterEach(() => {
 
 it("renders with title", () => {
   act(() => {
-    render(<Contact />, container);
+    render(
+      <MemoryRouter>
+        <Contact />
+      </MemoryRouter>, 
+      container
+    );
   });
   const title = container.querySelector('.contact-head-section h1');
   expect(title.textContent).toBe(`Let's talk.`);
@@ -28,7 +34,12 @@ it("renders with title", () => {
 
 it("renders with caption subtitle", () => {
   act(() => {
-    render(<Contact />, container);
+    render(
+      <MemoryRouter>
+        <Contact />
+      </MemoryRouter>, 
+      container
+    );
   });
   const title = container.querySelector('.contact-form-section h2');
   expect(title.textContent).toBe('We are located in Lower Downtown of your city.');
@@ -36,7 +47,12 @@ it("renders with caption subtitle", () => {
 
 it("renders with caption text", () => {
   act(() => {
-    render(<Contact />, container);
+    render(
+      <MemoryRouter>
+        <Contact />
+      </MemoryRouter>, 
+      container
+    );
   });
   const title = container.querySelector('.contact-form-section h4');
   expect(title.textContent).toBe('Ecstatic advanced and procured civility not absolute put continue. Overcame breeding or my concerns removing desirous so absolute. My melancholy unpleasing imprudence considered in advantages so impression.');
