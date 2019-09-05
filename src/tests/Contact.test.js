780c6135f22b8065a6a0e30f2402f1319379ab4e
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-09-05 15:10:11 +0200

    Add initial test for Contact component

---
+++ src/tests/Contact.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Contact from './../components/Contact/Contact';
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders with title", () => {
+  act(() => {
+    render(<Contact />, container);
+  });
+  const title = container.querySelector('.contact-head-section h1');
+  expect(title.textContent).toBe(`Let's talk.`);
+});
+
+it("renders with caption subtitle", () => {
+  act(() => {
+    render(<Contact />, container);
+  });
+  const title = container.querySelector('.contact-form-section h2');
+  expect(title.textContent).toBe('We are located in Lower Downtown of your city.');
+});
+
+it("renders with caption text", () => {
+  act(() => {
+    render(<Contact />, container);
+  });
+  const title = container.querySelector('.contact-form-section h4');
+  expect(title.textContent).toBe('Ecstatic advanced and procured civility not absolute put continue. Overcame breeding or my concerns removing desirous so absolute. My melancholy unpleasing imprudence considered in advantages so impression.');
+});
