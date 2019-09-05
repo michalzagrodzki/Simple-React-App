6080320fc6dae4b88b90684a5c8775ef8b6a8b5e
Michal Zagrodzki <michalzagrodzki@gmail.com>
2019-09-05 21:45:25 +0200

    Add some tests to Main component. Work in progress.

--- src/tests/Main.test.js
+++ src/tests/Main.test.js
@@ -45,6 +45,32 @@ it("renders with first section title", () => {
   expect(title.textContent).toBe('REACT APP');
 });
 
+it("renders with second section title", () => {
+  act(() => {
+    render(
+      <MemoryRouter>
+        <Main />
+      </MemoryRouter>, 
+      container
+    );
+  });
+  const title = container.querySelector('.product-header h4');
+  expect(title.textContent).toBe('Products');
+});
+
+it("renders with third section title", () => {
+  act(() => {
+    render(
+      <MemoryRouter>
+        <Main />
+      </MemoryRouter>, 
+      container
+    );
+  });
+  const title = container.querySelector('.contact-header h4');
+  expect(title.textContent).toBe('Contact');
+});
+
 it("renders with main message title", () => {
   act(() => {
     render(
@@ -69,4 +95,56 @@ it("renders with main message sub-title", () => {
   });
   const title = container.querySelector('.subtitle-body h3');
   expect(title.textContent).toBe('this app shows capabilities of using react in simple cases');
+});
+
+it("renders with product action button text", () => {
+  act(() => {
+    render(
+      <MemoryRouter>
+        <Main />
+      </MemoryRouter>, 
+      container
+    );
+  });
+  const button = container.querySelector('.products-footer button');
+  expect(button.textContent).toBe('MORE PRODUCTS');
+});
+
+it("renders with contact title", () => {
+  act(() => {
+    render(
+      <MemoryRouter>
+        <Main />
+      </MemoryRouter>, 
+      container
+    );
+  });
+  const title = container.querySelector('.contact-section h2');
+  expect(title.textContent).toBe('Take a contact with us');
+});
+
+it("renders with contact sub title", () => {
+  act(() => {
+    render(
+      <MemoryRouter>
+        <Main />
+      </MemoryRouter>, 
+      container
+    );
+  });
+  const title = container.querySelector('.contact-section h3');
+  expect(title.textContent).toBe('We are working with different people on different projects');
+});
+
+it("renders with contact submit button", () => {
+  act(() => {
+    render(
+      <MemoryRouter>
+        <Main />
+      </MemoryRouter>, 
+      container
+    );
+  });
+  const title = container.querySelector('.contact-section form button');
+  expect(title.textContent).toBe('SEND MESSAGE');
 });
